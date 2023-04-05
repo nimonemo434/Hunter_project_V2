495c7ed278ec37292cce97b2821e351883e79634
nimonemo434 <109446732+nimonemo434@users.noreply.github.com>
2023-04-06 07:58:50 +0900

    throwbomb
    throwbomb

---
+++ Unity/Project_hunter/Assets/Scenes/Scripts/PlayerFire.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFire : MonoBehaviour
+{
+    public GameObject firePosition;
+    public GameObject bombFactory;
+    public float throwPower = 15f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+            GameObject bomb = Instantiate(bombFactory);
+            bomb.transform.position = firePosition.transform.position;
+
+            Rigidbody rb = bomb.GetComponent<Rigidbody>();
+            rb.AddForce(Camera.main.transform.forward*throwPower, ForceMode.Impulse);
+        }
+    }
+}
